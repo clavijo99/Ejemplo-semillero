29ce59c7fa2cb22db86a2e8afe72e35cc9e30c3d
clavijo99 <bclavijogomez@gmail.com>
2026-04-21 20:55:41 -0500

    add button

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Hello World!'),
+              OutlinedButton(onPressed: (){}, child: Text('Soy un botton'))
+            ],
+          ),
         ),
       ),
     );
